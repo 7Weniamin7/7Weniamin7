7b83d648f31d2d0e70c8e05c2daa0893de4cd1ca
7Weniamin7 <88266791+7Weniamin7@users.noreply.github.com>
2021-08-01 12:38:48 +0500

    You my Calculator RU

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num, num_1, first;
+            string name;
+            Console.WriteLine("Введите первое число");
+
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+
+            num_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите наименование + - * /");
+            name = Convert.ToString(Console.ReadLine());
+
+            if (name == "+")
+            {
+                first = num + num_1;
+                Console.WriteLine(first);
+            }
+            if (name == "-")
+            {
+                first = num - num_1;
+                Console.WriteLine(first);
+            }
+            if (name == "*")
+            {
+                first = num * num_1;
+                Console.WriteLine(first);
+            }
+            if (name == "/")
+            {
+                first = num / num_1;
+                Console.WriteLine(first);
+            }
+            Console.ReadKey();
+        }
+    }
+}
